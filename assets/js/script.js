eb2e88c9aba3bc90471e8f7779b723c59d96fdfb
ShellyBrady <mchellebr@hotmail.com>
2022-09-07 11:30:55 +0000

    add comment labels to css for clarity and push changes to css button style and position

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,38 +7,4 @@ let
 
 
 
-//load the images and image cover
-for(var i = 1; i <= noOfBoxGame; i++){
-    boxes += "<div id='box-" + i + "' class='box-picture'><img src='game-images/" + (parseInt(boxIndexes[i-1]) + 1) + ".jpg'/></div>";
-    boxCover += "<div id='box-cover-" + i + "' class='box-cover' data-id='" + (parseInt(boxIndexes[i-1]) + 1) + "'></div>";
-}
-boxCover = "<div class='box-cover-wrapper'>" + boxCover + "</div>";
-$("#game-content").html(boxes + boxCover);
-
-//add event to click the box cover image
-$(".box-cover").off("click");
-$(".box-cover").on("click", function(){
-    if(noOfClick <= 1){
-        clickCounter++;
-        $("#no-of-clicks").html(clickCounter);
-        
-        noOfClick++;
-        $(this).addClass('animated flipOutX'); 
-        
-        var clickCover = {
-            ImageID: $(this).attr("data-id"),
-            CoverID: $(this).attr("id").replace("box-cover-","")
-        }
-        clickImages.push(clickCover);
-        
-        if(noOfClick >= 2){
-            //check if the revealed images are correct
-            if(clickImages[0].ImageID == clickImages[1].ImageID){
-                correctGuess++;
-                $("#correct-guess").html(correctGuess);
-                
-                //reset the variables
-                noOfClick = 0;
-                clickImages = [];
-                
 
